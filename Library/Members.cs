a34aed3bf75c81cdc17fb258eaccc4f82ea5e2ea
Webprivstu <beggs-g4@ulster.ac.uk>
2025-10-06 14:47:55 +0100

    26T

---
+++ Library/Members.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library
+{
+    class Members
+    {
+        private int memberID;
+        private string name;
+        private string address;
+        private int phone;
+    
+       
+          public int MemberID
+        {
+            get { return memberID; }
+            private set { memberID = value; }
+        }
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+        public string Address
+        {
+            get { return address; }
+            set { address = value; }
+        }
+
+        public int Phone
+        {
+            get { return phone; }
+            set { phone = value; }
+        }
+
+        public Members (int memberID, string name, string address, int phone)
+        {
+            this.memberID = memberID;
+            this.name = name;
+            this.address = address;
+            this.phone = phone;
+        }
+
+        public void DisplayInfo()
+        {
+            Console.WriteLine($"MemberID: {memberID}");
+            Console.WriteLine($"Name: {name}");
+            Console.WriteLine($"Address: {address}");
+            Console.WriteLine($"Phone Number: {phone}");
+            Console.WriteLine();
+        }
+
+    }
+}
--- Library/Program.cs
+++ Library/Program.cs
@@ -18,8 +18,19 @@ class Program
 
 
         //Output book information to the console
+        Console.WriteLine("Current Books");
         book.DisplayInfo();
         book1.DisplayInfo();
+
+        //member object 
+        Members member = new Members(1, "John Smith", "1 High Street", 0790090090);
+        //member object 1
+        Members member1 = new Members(2, "Mary Jones", "102 Garden Road", 0790345666);
+
+        //member display info
+        Console.WriteLine("Current library members");
+        member.DisplayInfo();
+        member1.DisplayInfo();
 
     }
 

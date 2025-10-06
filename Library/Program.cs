030b0ce67e0f37db0dac9f29f48687657eee819a
Webprivstu <beggs-g4@ulster.ac.uk>
2025-10-06 12:09:21 +0100

    class 21 T code

--- Library/Program.cs
+++ Library/Program.cs
@@ -1,1 +1,46 @@
-﻿
+﻿class Book ()
+{
+    //variable list
+
+    string Title;
+    string Author;
+    string ISBN;
+
+    void DisplayInfo() //display info 
+    {
+
+        Console.WriteLine($"Book Title: {Title}");
+        Console.WriteLine($"Book Author: {Author}");
+        Console.WriteLine($"Book ISBN: {ISBN}");
+        Console.WriteLine();
+
+    }
+
+    static void Main(string[] args)
+    {
+
+        //book object
+
+        Book book = new Book();
+
+        book.Title = "C# beginners";
+        book.Author = "BillGates";
+        book.ISBN = "1234578";
+
+        //book 1 object
+
+        Book book1 = new Book();
+
+        book1.Title = "C# Methods and Classes";
+        book1.Author = "Microsoft";
+        book1.ISBN = "55667788";
+
+        //Output book information to the console
+        book.DisplayInfo();
+        book1.DisplayInfo();
+
+    }
+
+}
+
+

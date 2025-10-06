f4de591a162840376f55eb68e85ef48373ef8a8c
Webprivstu <beggs-g4@ulster.ac.uk>
2025-10-06 12:46:21 +0100

    24t

---
+++ Library/Book.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library
+{
+    class Book
+    {
+        //variable list
+
+        string Title;
+        string Author;
+        string ISBN;
+
+        void DisplayInfo() //display info 
+        {
+
+            Console.WriteLine($"Book Title: {Title}");
+            Console.WriteLine($"Book Author: {Author}");
+            Console.WriteLine($"Book ISBN: {ISBN}");
+            Console.WriteLine();
+
+        }
+
+        //Book Constructor 
+        public Book(string bookTitle, string bookAuthor, string bookISBN)
+        {
+
+            this.Title = bookTitle;
+            this.Author = bookAuthor;
+            this.ISBN = bookISBN;
+
+        }
+
+
+    }
+}
--- Library/Program.cs
+++ Library/Program.cs
@@ -1,37 +1,15 @@
-﻿class Book 
+﻿using Library;
+
+
+class Program
 {
-    //variable list
-
-    string Title;
-    string Author;
-    string ISBN;
-
-    void DisplayInfo() //display info 
-    {
-
-        Console.WriteLine($"Book Title: {Title}");
-        Console.WriteLine($"Book Author: {Author}");
-        Console.WriteLine($"Book ISBN: {ISBN}");
-        Console.WriteLine();
-
-    }
-
-    //Book Constructor 
-    public Book(string bookTitle, string bookAuthor, string bookISBN)
-    {
-
-        this.Title = bookTitle;
-        this.Author = bookAuthor;    
-        this.ISBN = bookISBN;   
-
-    }
 
     static void Main(string[] args)
     {
 
         //book object
 
-        Book book = new Book("C# beginners", "Bill Gates","12345678" );
+        Book book = new Book("C# beginners", "Bill Gates", "12345678");
 
         //book 1 object
 
@@ -46,5 +24,7 @@
     }
 
 }
+
+
 
 

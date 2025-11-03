8bda8d7bae09998992846305b1cfbe8b609fe676
B00970775 <beggs-g4@ulster.ac.uk>
2025-11-03 15:31:55 +0000

    week 7

--- Library/Program.cs
+++ Library/Program.cs
@@ -1,4 +1,5 @@
 ﻿using Library;
+using System.ComponentModel.DataAnnotations;
 
 
 class Program
@@ -6,31 +7,46 @@ class Program
 
     static void Main(string[] args)
     {
+        
+        // lists to store books and members
 
-        //book object
+        List<Book> books = new List<Book>();
+        List<Members> members = new List<Members>();
 
-        Book book = new Book("C# beginners", "Bill Gates", "12345678");
+        // create book object
 
-        //book 1 object
+        books.Add(new Book("C# beginners", "Bill Gates", "12345678"));
 
-        Book book1 = new Book("C# Methods and Classes", "Microsoft", "55667788");
+        //create book 1 object
+
+        books.Add(new Book("C# Methods and Classes", "Microsoft", "55667788"));
 
 
 
         //Output book information to the console
         Console.WriteLine("Current Books");
-        book.DisplayInfo();
-        book1.DisplayInfo();
+        //book.DisplayInfo();
+        //book1.DisplayInfo();
 
-        //member object 
-        Members member = new Members(1, "John Smith", "1 High Street", 0790090090);
-        //member object 1
-        Members member1 = new Members(2, "Mary Jones", "102 Garden Road", 0790345666);
+        //create member object 
+        members.Add(new Members(1, "John Smith", "1 High Street", 0790090090));
+        //create member object 1
+        members.Add(new Members(2, "Mary Jones", "102 Garden Road", 0790345666));
 
         //member display info
         Console.WriteLine("Current library members");
-        member.DisplayInfo();
-        member1.DisplayInfo();
+        //member.DisplayInfo();
+        //member1.DisplayInfo();
+        foreach (Members member in members)
+        {
+            member.DisplayInfo();
+
+        }
+
+        foreach (Book book in books)
+        {
+            book.DisplayInfo();
+        }
 
     }
 
